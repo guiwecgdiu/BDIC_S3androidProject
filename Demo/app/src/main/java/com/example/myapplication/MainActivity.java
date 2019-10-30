0f02d6ca00078532a88440284e0cbfd0b925d3c2
Arno Li <guiwecgdiu@163.com>
2019-10-30 09:04:01 +0800

    Dploy All Update. FtpDemo 和Demo并行测试开发

--- Demo/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Demo/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,80 @@
 package com.example.myapplication;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.myapplication.SFTPUtilis.SFTPUtils;
+
+
+public class MainActivity extends Activity implements View.OnClickListener {
+    private final String TAG = "MainActivity";
+    private Button buttonUpLoad = null;
+    private Button buttonDownLoad = null;
+    private SFTPUtils sftp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sftpmain);
+        init();
+    }
+
+    public void init() {
+        //获取控件对象
+        buttonUpLoad = (Button) findViewById(R.id.button_upload);
+        buttonDownLoad = (Button) findViewById(R.id.button_download);
+        //设置控件对应相应函数
+        buttonUpLoad.setOnClickListener(this);
+        buttonDownLoad.setOnClickListener(this);
+        //sftp = new SFTPUtils("SFTP服务器IP", "用户名", "密码");
+        sftp = new SFTPUtils("119.3.238.156", "siteadmin", "L1l2l3l4");
+    }
+
+    @Override
+    public void onClick(final View v) {
+            // TODO Auto-generated method stub
+            new Thread() {
+                @Override
+                public void run() {
+                    //这里写入子线程需要做的工作
+
+                    switch (v.getId()) {
+                        case R.id.button_upload: {
+                            //上传文件
+                            Log.d(TAG, "上传文件");
+                            String localPath = "sdcard/xml/";
+                            String remotePath = "test";
+                            sftp.connect();
+                            Log.d(TAG, "连接成功");
+                            sftp.uploadFile(remotePath, "APPInfo.xml", localPath, "APPInfo.xml");
+                            Log.d(TAG, "上传成功");
+                            sftp.disconnect();
+                            Log.d(TAG, "断开连接");
+                        }
+                        break;
+
+                        case R.id.button_download: {
+                            //下载文件
+                            Log.d(TAG, "下载文件");
+                            String localPath = "sdcard/download/";
+                            String remotePath = "test";
+                            sftp.connect();
+                            Log.d(TAG, "连接成功");
+                            sftp.downloadFile(remotePath, "APPInfo.xml", localPath, "APPInfo.xml");
+                            Log.d(TAG, "下载成功");
+                            sftp.disconnect();
+                            Log.d(TAG, "断开连接");
+
+                        }
+                        break;
+                        default:
+                            break;
+                    }
+                }
+            }.start();
     }
 }
